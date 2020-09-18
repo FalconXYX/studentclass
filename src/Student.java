38b3190b794ed7505c5a68865e4a0690b1c0ad77
part8880 <part8880@ISS2TYQKH2.scdsb.pri>
2020-09-18 12:27:05 -0400

    stuff

--- src/Student.java
+++ src/Student.java
@@ -29,10 +29,26 @@ public class Student {
     public int getScore(int i){ 
         if(i == 1){return test1;}
         if(i == 2){return test2;}
-        else{return test3; }
+        else{return test3; }}
     public int getavg(){
         int average;
         average = (int) Math.round((test1 + test2 + test3) / 3.0);
         return average; } 
+    public int getHighScore(){
+        int highScore;
+        highScore = test1;
+        if (test2 > highScore) highScore = test2;
+        if (test3 > highScore) highScore = test3;
+        return highScore; 
+              
+    }
+    public String toString(){
+    String str;
+    str = "Name:";
+    str+= "Test 1:";
+    str+="Test 2:";
+    str+="Test 3:";
+    str+="Average:";
+    return str;}
 
 }
